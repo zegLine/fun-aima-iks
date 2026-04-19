a831a56c68e88c44d294a183b58046c6afd4b616
Eugen Zdrincu <eugenzdrincu@gmail.com>
2026-04-20 00:47:06 +0300

    base for exercise 4

---
+++ src/eugene/osm/agent/online/ExtendedOnlineAgentOsmApp.java
@@ -0,0 +1,12 @@
+package eugene.osm.agent.online;
+
+import aimax.osm.gui.fx.applications.OnlineAgentOsmApp;
+
+public class ExtendedOnlineAgentOsmApp extends OnlineAgentOsmApp {
+
+    @Override
+    public String getTitle() {
+        return "Eugene's Online Agent OSM App";
+    }
+
+}
