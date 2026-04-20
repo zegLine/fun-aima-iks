1cd6e87a3e0b35628954e670c1d09ccbdf021d63
Eugen Zdrincu <eugenzdrincu@gmail.com>
2026-04-20 13:54:42 +0300

    exercise 4: 180 degree turn ban improvement

--- src/eugene/osm/agent/online/ExtendedOnlineAgentOsmApp.java
+++ src/eugene/osm/agent/online/ExtendedOnlineAgentOsmApp.java
@@ -1,12 +1,38 @@
 package eugene.osm.agent.online;
 
+import aima.core.agent.Agent;
+import aima.core.agent.impl.DynamicPercept;
+import aima.core.environment.map.BidirectionalMapProblem;
+import aima.core.environment.map.MapFunctions;
+import aima.core.environment.map.MoveToAction;
+import aima.core.search.framework.problem.GeneralProblem;
+import aima.core.search.framework.problem.OnlineSearchProblem;
+import aima.core.search.framework.problem.Problem;
 import aimax.osm.gui.fx.applications.OnlineAgentOsmApp;
 
+import java.util.List;
+import java.util.function.ToDoubleFunction;
+
 public class ExtendedOnlineAgentOsmApp extends OnlineAgentOsmApp {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     @Override
     public String getTitle() {
         return "Eugene's Online Agent OSM App";
+    }
+
+    @Override
+    protected Agent<DynamicPercept, MoveToAction> createAgent(List<String> locations) {
+        String goal = locations.get(1);
+        Problem<String, MoveToAction> p = new BidirectionalMapProblem(map, null, goal);
+        OnlineSearchProblem<String, MoveToAction> osp = new GeneralProblem<>
+                (null, p::getActions, null, p::testGoal, p::getStepCosts);
+        ToDoubleFunction<String> heuristic = state -> MapFunctions.getSLD(state, goal, map);
+
+        return new ImprovedLRTAStarAgent<>(osp, MapFunctions.createPerceptToStateFunction(), heuristic);
     }
 
 }
